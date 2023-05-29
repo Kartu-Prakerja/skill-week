231a716d980399716007162395a93c75b0dc82f9
therexxar <therexxar@gmail.com>
2023-05-29 16:14:32 +0700

    update javascript information

--- js/function.js
+++ js/function.js
@@ -2,8 +2,21 @@
 const loadItem = 12;
 var currentPage = 1;
 
+var emptyState = "<div class='col-12 col-md-9'>" +
+    "<div class='alert alert-info' role='alert'>" +
+        "<div class='d-flex'>" +
+            "<div class='pe-3'><i class='bi bi-info-circle-fill fs-4'></i></div>" +
+            "<div>"+
+                "<h6 class='alert-heading'>Pelatihan tidak ditemukan</h6>" +
+                "<p>Mohon periksa kembali kata kunci Anda dan pastikan ejaan dan tata bahasa yang benar. Anda juga dapat mencoba menggunakan kata kunci yang berbeda atau mencari di topik pelatihan yang berbeda.</p>"+
+            "</div>"+
+        "</div>"+
+    "</div>"+
+    "</div>";
+
 /** function load course */
 var templateCourse = function(target, data){ 
+    // temporary remove detail pelatihan "<a href='"+ data.course_url +"?utm_source=skillsweek&utm_medium=landing-page&utm_content=button' class='see-detail-course me-2 link-secondary' target='_blank' rel='nofollow' data-event='skill_week_click_course_detail text-link'>Deskripsi Pelatihan</a>" +
     var pills = data.course_type == "Online - LMS" ? "text-bg-warning" : "text-bg-help";
     var course_form_request = 'https://docs.google.com/forms/d/e/1FAIpQLScc3v4je6bcRHA_0H5ItpjaY_x8ump5K9pdc27ylti4pQo0xQ/viewform?usp=pp_url&entry.841678428=' + data.course_title.split(" ").join("+");
     var template = "<div class='col-12 col-md-6 col-xl-4 col-xxl-3 mb-4 mb-lg-5'>" +
@@ -26,7 +39,6 @@ var templateCourse = function(target, data){
                 "</div>" +
                 "<div class='mt-3 text-center'>" +
                     "<a href='"+ course_form_request +"&utm_source=skillsweek&utm_medium=landing-page&utm_content=button' class='apply-course btn btn-primary w-100 mb-2' target='_blank' rel='nofollow' data-event='skill_week_apply_course'>Dapatkan Voucher Pelatihan</a>" +
-                    "<a href='"+ data.course_url +"?utm_source=skillsweek&utm_medium=landing-page&utm_content=button' class='see-detail-course me-2 link-secondary' target='_blank' rel='nofollow' data-event='skill_week_click_course_detail text-link'>Deskripsi Pelatihan</a>" +
                 '</div>'
             "</div>" +
         "</div>" +
@@ -73,15 +85,20 @@ var filterSelect = function(target, data, start, end) {
 
         appendTarget.html('');
         
-        // implement append data
-        $.each(dataKeyword.slice(start, end), function(i, list) {
-            templateCourse(appendTarget, list);
-        });
+        if (dataKeyword.length !== 0) {
+            // implement append data
+            $.each(dataKeyword.slice(start, end), function(i, list) {
+                templateCourse(appendTarget, list);
+            });
+        } else {
+            appendTarget.html(emptyState);
+        }
 
         btnLoadMore(loadMoreTarget, loadItem, start, end, dataKeyword, appendTarget, currentPage, paging);
         checkLoadMore(loadMoreTarget, paging, currentPage);
     })
 }
+
 
 var filterKeyword = function(formSeaerch, buttonSearch, data, start, end) {
     $(formSeaerch).on('submit', function(e) { 
@@ -99,7 +116,6 @@ var filterKeyword = function(formSeaerch, buttonSearch, data, start, end) {
         } else {
             var dataKeyword = _.filter(data, function(list) { return list.course_title.toLowerCase().indexOf(keyword.toLowerCase()) !== -1; })
         }
-        console.log(dataKeyword)
 
         // define pagination
         var dataLength = dataKeyword.length;
@@ -107,10 +123,14 @@ var filterKeyword = function(formSeaerch, buttonSearch, data, start, end) {
 
         appendTarget.html('');
         
-        // implement append data
-        $.each(dataKeyword.slice(start, end), function(i, list) {
-            templateCourse(appendTarget, list);
-        });
+        if (dataKeyword.length !== 0) {
+            // implement append data
+            $.each(dataKeyword.slice(start, end), function(i, list) {
+                templateCourse(appendTarget, list);
+            });
+        } else {
+            appendTarget.html(emptyState);
+        }
 
         btnLoadMore(loadMoreTarget, loadItem, start, end, dataKeyword, appendTarget, currentPage, paging);
         checkLoadMore(loadMoreTarget, paging, currentPage);
