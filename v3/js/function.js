8438ab9f04d15e71570e92e45443e7dd5a04f90e
Prakerja <prakerja@Prakerjas-MacBook-Pro-6.local>
2023-10-11 15:54:21 +0700

    add share native button

--- v3/js/function.js
+++ v3/js/function.js
@@ -628,3 +628,31 @@ function courseLoaderHome() {
     // run init course home
     courseLoaderHome();
  })(jQuery);
+
+
+function shareCourse(){
+    const shareButton = document.querySelector('.share-button');
+    const shareDialog = document.querySelector('.share-dialog');
+    const closeButton = document.querySelector('.close-button');
+    
+    shareButton.addEventListener('click', event => {
+    if (navigator.share) { 
+        navigator.share({
+        title: 'WebShare API Demo',
+        url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+        }).then(() => {
+        console.log('Thanks for sharing!');
+        })
+        .catch(console.error);
+        } else {
+            shareDialog.classList.add('is-open');
+        }
+    });
+    
+    closeButton.addEventListener('click', event => {
+    shareDialog.classList.remove('is-open');
+    });
+}
+
+shareCourse();
+ 
