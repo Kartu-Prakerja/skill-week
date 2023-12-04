aeced3eed84a13998e0c9ef77ca9232074e25a8f
Prakerja <prakerja@Prakerjas-MacBook-Pro-6.local>
2023-12-04 12:08:20 +0700

    add search & Profil Saya

--- js/function.js
+++ js/function.js
@@ -1186,6 +1186,17 @@ function homeCheckLogin() {
         $('body').removeClass('freeze');
     });
 
+    // Search Modal on header 
+    $('.modal-search-trigger').click (function(){
+        $('.modal-search').toggleClass('is-show');
+        $('body').toggleClass('freeze');
+      });
+
+      $('.modal-search-close').click (function(){
+        $('.modal-search').toggleClass('is-show');
+        $('body').toggleClass('freeze');
+      });
+
     
     // $('#coworkingCarousel').owlCarousel({
     //     loop:true,
@@ -1335,122 +1346,122 @@ function homeCheckLogin() {
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
-( function( window ) {
+// ( function( window ) {
 
-    'use strict';
+//     'use strict';
     
-    // class helper functions from bonzo https://github.com/ded/bonzo
+//     // class helper functions from bonzo https://github.com/ded/bonzo
     
-    function classReg( className ) {
-      return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-    }
+//     function classReg( className ) {
+//       return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+//     }
     
-    // classList support for class management
-    // altho to be fair, the api sucks because it won't accept multiple classes at once
-    var hasClass, addClass, removeClass;
+//     // classList support for class management
+//     // altho to be fair, the api sucks because it won't accept multiple classes at once
+//     var hasClass, addClass, removeClass;
     
-    if ( 'classList' in document.documentElement ) {
-      hasClass = function( elem, c ) {
-        return elem.classList.contains( c );
-      };
-      addClass = function( elem, c ) {
-        elem.classList.add( c );
-      };
-      removeClass = function( elem, c ) {
-        elem.classList.remove( c );
-      };
-    }
-    else {
-      hasClass = function( elem, c ) {
-        return classReg( c ).test( elem.className );
-      };
-      addClass = function( elem, c ) {
-        if ( !hasClass( elem, c ) ) {
-          elem.className = elem.className + ' ' + c;
-        }
-      };
-      removeClass = function( elem, c ) {
-        elem.className = elem.className.replace( classReg( c ), ' ' );
-      };
-    }
+//     if ( 'classList' in document.documentElement ) {
+//       hasClass = function( elem, c ) {
+//         return elem.classList.contains( c );
+//       };
+//       addClass = function( elem, c ) {
+//         elem.classList.add( c );
+//       };
+//       removeClass = function( elem, c ) {
+//         elem.classList.remove( c );
+//       };
+//     }
+//     else {
+//       hasClass = function( elem, c ) {
+//         return classReg( c ).test( elem.className );
+//       };
+//       addClass = function( elem, c ) {
+//         if ( !hasClass( elem, c ) ) {
+//           elem.className = elem.className + ' ' + c;
+//         }
+//       };
+//       removeClass = function( elem, c ) {
+//         elem.className = elem.className.replace( classReg( c ), ' ' );
+//       };
+//     }
     
-    function toggleClass( elem, c ) {
-      var fn = hasClass( elem, c ) ? removeClass : addClass;
-      fn( elem, c );
-    }
+//     function toggleClass( elem, c ) {
+//       var fn = hasClass( elem, c ) ? removeClass : addClass;
+//       fn( elem, c );
+//     }
     
-    var classie = {
-      // full names
-      hasClass: hasClass,
-      addClass: addClass,
-      removeClass: removeClass,
-      toggleClass: toggleClass,
-      // short names
-      has: hasClass,
-      add: addClass,
-      remove: removeClass,
-      toggle: toggleClass
-    };
+//     var classie = {
+//       // full names
+//       hasClass: hasClass,
+//       addClass: addClass,
+//       removeClass: removeClass,
+//       toggleClass: toggleClass,
+//       // short names
+//       has: hasClass,
+//       add: addClass,
+//       remove: removeClass,
+//       toggle: toggleClass
+//     };
     
-    // transport
-    if ( typeof define === 'function' && define.amd ) {
-      // AMD
-      define( classie );
-    } else {
-      // browser global
-      window.classie = classie;
-    }
+//     // transport
+//     if ( typeof define === 'function' && define.amd ) {
+//       // AMD
+//       define( classie );
+//     } else {
+//       // browser global
+//       window.classie = classie;
+//     }
     
-    })( window );
+//     })( window );
     
 
-    (function() {
-        var morphSearch = document.getElementById( 'morphsearch' ),
-            input = morphSearch.querySelector( 'input.morphsearch-input' ),
-            ctrlClose = morphSearch.querySelector( 'span.morphsearch-close' ),
-            isOpen = isAnimating = false,
-            // show/hide search area
-            toggleSearch = function(evt) {
-                // return if open and the input gets focused
-                if( evt.type.toLowerCase() === 'focus' && isOpen ) return false;
+//     (function() {
+//         var morphSearch = document.getElementById( 'morphsearch' ),
+//             input = morphSearch.querySelector( 'input.morphsearch-input' ),
+//             ctrlClose = morphSearch.querySelector( 'span.morphsearch-close' ),
+//             isOpen = isAnimating = false,
+//             // show/hide search area
+//             toggleSearch = function(evt) {
+//                 // return if open and the input gets focused
+//                 if( evt.type.toLowerCase() === 'focus' && isOpen ) return false;
 
-                if( isOpen ) {
-                    classie.remove( morphSearch, 'open' );
+//                 if( isOpen ) {
+//                     classie.remove( morphSearch, 'open' );
 
-                    // trick to hide input text once the search overlay closes 
-                    // todo: hardcoded times, should be done after transition ends
-                    if( input.value !== '' ) {
-                        setTimeout(function() {
-                            classie.add( morphSearch, 'hideInput' );
-                            setTimeout(function() {
-                                classie.remove( morphSearch, 'hideInput' );
-                                input.value = '';
-                            }, 300 );
-                        }, 500);
-                    }
+//                     // trick to hide input text once the search overlay closes 
+//                     // todo: hardcoded times, should be done after transition ends
+//                     if( input.value !== '' ) {
+//                         setTimeout(function() {
+//                             classie.add( morphSearch, 'hideInput' );
+//                             setTimeout(function() {
+//                                 classie.remove( morphSearch, 'hideInput' );
+//                                 input.value = '';
+//                             }, 300 );
+//                         }, 500);
+//                     }
                     
-                    input.blur();
-                }
-                else {
-                    classie.add( morphSearch, 'open' );
-                }
-                isOpen = !isOpen;
-            };
+//                     input.blur();
+//                 }
+//                 else {
+//                     classie.add( morphSearch, 'open' );
+//                 }
+//                 isOpen = !isOpen;
+//             };
 
-        // events
-        input.addEventListener( 'focus', toggleSearch );
-        ctrlClose.addEventListener( 'click', toggleSearch );
-        // esc key closes search overlay
-        // keyboard navigation events
-        document.addEventListener( 'keydown', function( ev ) {
-            var keyCode = ev.keyCode || ev.which;
-            if( keyCode === 27 && isOpen ) {
-                toggleSearch(ev);
-            }
-        } );
+//         // events
+//         input.addEventListener( 'focus', toggleSearch );
+//         ctrlClose.addEventListener( 'click', toggleSearch );
+//         // esc key closes search overlay
+//         // keyboard navigation events
+//         document.addEventListener( 'keydown', function( ev ) {
+//             var keyCode = ev.keyCode || ev.which;
+//             if( keyCode === 27 && isOpen ) {
+//                 toggleSearch(ev);
+//             }
+//         } );
 
 
-        /***** for demo purposes only: don't allow to submit the form *****/
-        morphSearch.querySelector( 'button[type="submit"]' ).addEventListener( 'click', function(ev) { ev.preventDefault(); } );
-    })();
+//         /***** for demo purposes only: don't allow to submit the form *****/
+//         morphSearch.querySelector( 'button[type="submit"]' ).addEventListener( 'click', function(ev) { ev.preventDefault(); } );
+//     })();
 
